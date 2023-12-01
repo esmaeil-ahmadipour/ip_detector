ea879e1c123a9415f15dbcaf3584182e6639278e
esmail.ahmadipour <esmaeil.ahmadipour@samim.net>
2023-12-01 17:47:39 +0330

    refactor(app): Simplifying example code and enhance documentation. #3
    - refactor the example project and make it easier to understand.
    - refactor `README.md` file for new example code.
    - add GIF animation to `README.md` file.
    - add test case files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,14 @@
+## 0.0.2
+
+- ### Simplifying example code and enhance documentation. ([Issue](https://github.com/esmaeil-ahmadipour/ip_detector/issues/3) / [Merge Request](https://github.com/esmaeil-ahmadipour/ip_detector/pull/4))
+  - refactor the example project and make it easier to understand.
+  - refactor `README.md` file for new example code.
+  - add GIF animation to `README.md` file.
+  - add test case files.
+
 ## 0.0.1
 
-- ### Initial release. ([Issue #1](https://github.com/esmaeil-ahmadipour/ip_detector/issues/1) / [Merge Request #2](https://github.com/esmaeil-ahmadipour/ip_detector/pull/2))
+- ### Initial release. ([Issue](https://github.com/esmaeil-ahmadipour/ip_detector/issues/1) / [Merge Request](https://github.com/esmaeil-ahmadipour/ip_detector/pull/2))
     - add MIT license
     - add README.md
     - add example project
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,79 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:ip_detector/ip_detector.dart';
 
+/// ## [DemoScreen] Class Documentation:
+///
+/// The `IpDetectorWidget` class is a Flutter `StatefulWidget` that demonstrates the usage of the `IpDetector` class for fetching IP-related information and handling different states.
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      useMaterial3: true,
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+            backgroundColor: Colors.deepPurple.shade900,
+            foregroundColor: Colors.white),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple.shade900),
+        useMaterial3: true,
+      ),
+      home: const DemoScreen(),
     ),
-    home: const MyHomePage(),
-  ));
+  );
 }
 
-/// # [MyHomePage] Widget Class :
+/// ## [DemoScreen] Class Documentation:
 ///
-/// ## Overview
-/// The `MyHomePage` class is a stateful widget that represents the main home page of the Flutter application.
-/// It utilizes the `IpDetector` class to fetch information about the user's IP address.
-/// The UI dynamically adapts based on the response received from the API.
+/// The `IpDetectorWidget` class is a Flutter `StatefulWidget` that demonstrates the usage of the `IpDetector` class for fetching IP-related information and handling different states.
 ///
-/// ## Properties
-/// - **ipDetector**: An instance of the `IpDetector` class used to manage and retrieve IP-related information.
+/// ### Class Overview:
 ///
-/// ## Methods
-/// - **initState**: Initializes the state of the widget, creating an instance of `IpDetector` with a specified timeout duration.
-/// - **build**: Builds the widget tree based on the current state, displaying a Scaffold with an AppBar and a body containing a FutureBuilder.
+/// - This class displays a screen with IP-related information fetched using the `IpDetector` class.
+/// - It demonstrates the use of `FutureBuilder` to handle different states (Waiting, Succeed, and Error) during the IP detection process.
+/// - The screen includes a retry button for error states to allow users to attempt IP detection again.
 ///
-/// ## `build` Method Logic
-/// 1. **AppBar**: Displays an AppBar with a title.
-/// 2. **Body**: Utilizes a `FutureBuilder` to handle the asynchronous IP address fetch operation.
-///    - **Loading State**: Displays a circular progress indicator while waiting for the response.
-///    - **Done State**: Determines the UI based on the type of response.
-///      - **Succeed Response**: Calls the `buildSucceedUI` method to display a column with information about the country, city, and IP address.
-///      - **Other States**: Calls the `buildOtherThanSucceedUI` method to display a retry button.
-///
-/// ## Helper Methods
-/// ### 1. `buildSucceedUI`
-/// - **Description**: Builds a column widget containing information about the country, city, and IP address.
-/// - **Return Type**: Widget
-///
-/// ### 2. `buildOtherThanSucceedUI`
-/// - **Description**: Builds a center-aligned widget containing the `RetryButton`.
-/// - **Return Type**: Widget
-///
-/// ## Widget Documentation
-/// ### `RetryButton` Widget
-/// - **Description**: A `FloatingActionButton` with an icon for retrying an operation.
-/// - **Properties**:
-///   - `onPress`: A callback function triggered when the button is pressed.
-///
-/// ### `TextWidget` Widget
-/// - **Description**: A customizable text widget based on the title and font size.
-/// - **Properties**:
-///   - `title`: The text content to be displayed.
-///   - `largeText`: A boolean indicating whether to use a large font size (default is false).
-///
-/// This `MyHomePage` class creates a responsive and dynamic user interface based on the asynchronous IP address fetch operation,
-/// enhancing the user experience by displaying relevant information or providing the option to retry the operation in case of failure.
-/// The `RetryButton` and `TextWidget` widgets contribute to the modularity and readability of the code.
-///
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class DemoScreen extends StatefulWidget {
+  const DemoScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DemoScreen> createState() => _DemoScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  late IpDetector ipDetector;
+/// ### [_DemoScreenState] Class Overview:
+///
+/// - This class is part of the state management for the [DemoScreen] widget.
+/// - It initializes an instance of the `IpDetector` class in the `initState` method with a specified timeout duration of one second.
+/// - The chosen timeout duration is intentionally short for testing and observing timeout scenarios during IP detection.
+///
+/// ### Properties:
+///
+/// - [ipDetector] : An instance of the `IpDetector` class used for fetching IP-related information.
+/// - [ipDetectionFuture] : A future representing the IP detection process, used in the `FutureBuilder`.
+///
+/// ### Initialization:
+///
+/// - The [initState] method is overridden to create an instance of the `IpDetector` class during the widget's initialization.
+/// - The chosen timeout duration of one second ensures a relatively short wait time during the IP detection process.
+///
+class _DemoScreenState extends State<DemoScreen> {
+  late final IpDetector ipDetector;
+  Future<IpDetectorResponseModel>? ipDetectionFuture;
 
   @override
   void initState() {
-    //  creates an instance of the `IpDetector` class.
-    // The chosen timeout duration of one second is intentionally short for testing and observing timeout scenarios.
-    ipDetector = IpDetector(timeout: const Duration(seconds: 1));
+    /// Creates an instance of the `IpDetector` class.
+    /// The chosen timeout duration of ten seconds is intentionally set for testing and observing timeout scenarios.
+    ipDetector = IpDetector(timeout: const Duration(seconds: 10));
+
+    /// Initiates the IP detection process and assigns the resulting future to `ipDetectionFuture`.
+    ipDetectionFuture = ipDetector.fetch(enableLog: true);
     super.initState();
   }
 
@@ -81,124 +73,54 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
+        title: const Text("DemoScreen"),
       ),
+      // FutureBuilder Widget : Handles different states (Waiting, Succeed, and Error) during the IP detection process.
       body: FutureBuilder(
-          future: ipDetector.fetch(enableLog: true),
-          builder: (context, snapshot) {
-            // when request send and getting data from api (may be get error/succeed state)
-
-            if (snapshot.hasData &&
-                snapshot.connectionState == ConnectionState.done) {
-              // for show succeed ui , we can limit state on succeed like below conditions.
-
-              if (snapshot.data!.type ==
-                  IpDetectorResponseType.succeedResponse) {
-                // we can show succeed widget in this place .
-
-                return buildSucceedUI();
-              } else {
-                // we can show other widget for any state in this place.
-                // another states may be one of below list :
-                //   `IpDetectorResponseType.failedResponse`
-                //   `IpDetectorResponseType.networkIssue`
-                //   `IpDetectorResponseType.timeOutIssue`
-                //   `IpDetectorResponseType.formatExceptionIssue`
-                //   `IpDetectorResponseType.otherErrors`
-                // in this sample code below ui (below ui is an retry button) used for any state except `IpDetectorResponseType.succeedResponse` .
-
-                return buildOtherThanSucceedUI();
-              }
+        future: ipDetectionFuture,
+        builder: (context, snapshot) {
+          if (snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.done) {
+            // This code scope includes `Succeed` and `Error` states.
+            if (snapshot.data!.type == IpDetectorResponseType.succeedResponse) {
+              // This code scope is `Succeed` state , and `ListView` widget displays IP-related information when the detection is successful.
+              return ListView(
+                children: [
+                  Text('Country : ${ipDetector.country()}'),
+                  Text('Region Name : ${ipDetector.regionName()}'),
+                  Text('City : ${ipDetector.city()}'),
+                  Text('IP : ${ipDetector.query()}'),
+                  Text('ISP : ${ipDetector.isp()}'),
+                  Text('Latitude: ${ipDetector.lat()}'),
+                  Text('Longitude : ${ipDetector.lon()}'),
+                  Text('Time Zone : ${ipDetector.timezone()}'),
+                ],
+              );
             } else {
-              // when request send and waiting for response
-              // we can show loading widget in this place .
-
-              return const Center(child: CircularProgressIndicator());
+              // This code scope is `Error` state , and `MaterialButton` widget displays a retry button for error states to allow users to attempt IP detection again.
+              return Center(
+                child: MaterialButton(
+                    shape: const StadiumBorder(),
+                    color: Theme.of(context).primaryColor,
+                    child: const Text('RETRY BUTTON'),
+                    onPressed: () {
+                      ipDetectionFuture = ipDetector.fetch(enableLog: true);
+                      setState(() {});
+                    }),
+              );
             }
-          }),
-    );
-  }
-
-  // Helper method to build UI for states other than succeed response.
-
-  Widget buildOtherThanSucceedUI() {
-    return Center(
-      child: RetryButton(
-        onPress: () {
-          setState(() {});
+          } else {
+            // This code scope is `Waiting` State , and `CircularProgressIndicator` widget shown during the waiting state.
+            return const Center(child: CircularProgressIndicator());
+          }
         },
       ),
-    );
-  }
-
-  // Helper method to build UI for succeed response.
-
-  Widget buildSucceedUI() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        const TextWidget('Country :', largeText: true),
-        TextWidget('${ipDetector.country()}'),
-        const Divider(height: 32),
-        const TextWidget(
-          'City :',
-          largeText: true,
-        ),
-        TextWidget('${ipDetector.city()}'),
-        const Divider(height: 32),
-        const TextWidget('IP :', largeText: true),
-        TextWidget('${ipDetector.query()}'),
-        Container(
-          padding: const EdgeInsetsDirectional.only(end: 16),
-          alignment: AlignmentDirectional.bottomEnd,
-          child: RetryButton(
-            onPress: () {
-              setState(() {});
-            },
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-/// ## [RetryButton] Widget Class:
-///
-/// A FloatingActionButton with an icon for retrying an operation.
-///
-class RetryButton extends StatelessWidget {
-  const RetryButton({super.key, this.onPress});
-
-  final Function()? onPress;
-
-  @override
-  Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: onPress,
-      child: const Icon(Icons.refresh),
-    );
-  }
-}
-
-/// ## [TextWidget] Widget Class:
-///
-/// A customizable Text widget based on title and font size..
-///
-class TextWidget extends StatelessWidget {
-  const TextWidget(this.title, {super.key, largeText});
-
-  final String title;
-  final bool? largeText = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      title,
-      style: largeText!
-          ? Theme.of(context).textTheme.headlineLarge
-          : Theme.of(context).textTheme.headlineSmall,
+      floatingActionButton: FloatingActionButton(
+          child: const Text('RETRY'),
+          onPressed: () {
+            ipDetectionFuture = ipDetector.fetch(enableLog: true);
+            setState(() {});
+          }),
     );
   }
 }
